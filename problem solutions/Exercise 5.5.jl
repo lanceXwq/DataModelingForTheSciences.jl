b731df74799a1f8634c7829fafcd6d43807dece0
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2022-03-07 11:03:32 -0700

    modify 5.5

--- problem solutions/Exercise 5.5.jl
+++ problem solutions/Exercise 5.5.jl
@@ -6,6 +6,7 @@ using InteractiveUtils
 
 # ╔═╡ ed26dde0-9c07-11ec-33fb-9b4d9ed3a144
 md"
+First, note that Barker's ratio is always smaller than 1, so $\alpha_r(r') = A_r(r')$. 
 Since 
 ```math
 \pi(r)\alpha_r(r')Q_r(r')
@@ -13,7 +14,6 @@ Since
 =\pi(r')\alpha_{r'}(r)Q_{r'}(r),
 ```
 all the derivation in the textbook is still valid.
-
 "
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
